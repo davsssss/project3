6508b5de6376f33a4d48f43dd60c2ba6ca4cd9fc
Alex Clayton <50820839+Necronics@users.noreply.github.com>
2023-10-02 21:30:19 -0700

    Chart files
    data csv is outdated

---
+++ pieChartData.js
@@ -0,0 +1,269 @@
+var pieData = {
+    "Argentina": {
+        "Comedy": 3,
+        "Education": 1,
+        "Entertainment": 3,
+        "Film & Animation": 1,
+        "Gaming": 1,
+        "Music": 4
+    },
+    "Australia": {
+        "Education": 1,
+        "Gaming": 2,
+        "Howto & Style": 2,
+        "Music": 2,
+        "Sports": 1
+    },
+    "Bangladesh": {
+        "News & Politics": 1
+    },
+    "Barbados": {
+        "Entertainment": 1
+    },
+    "Brazil": {
+        "Autos & Vehicles": 1,
+        "Comedy": 7,
+        "Entertainment": 14,
+        "Film & Animation": 7,
+        "Gaming": 6,
+        "Howto & Style": 2,
+        "Music": 15,
+        "Nonprofits & Activism": 1,
+        "People & Blogs": 6,
+        "Sports": 1
+    },
+    "Canada": {
+        "Comedy": 1,
+        "Education": 1,
+        "Entertainment": 2,
+        "Gaming": 3,
+        "Music": 4,
+        "Science & Technology": 2
+    },
+    "Chile": {
+        "Gaming": 3
+    },
+    "China": {
+        "Howto & Style": 1
+    },
+    "Colombia": {
+        "Comedy": 1,
+        "Entertainment": 1,
+        "Music": 7,
+        "People & Blogs": 2
+    },
+    "Cuba": {
+        "Gaming": 1
+    },
+    "Ecuador": {
+        "Entertainment": 1,
+        "People & Blogs": 1
+    },
+    "Egypt": {
+        "Howto & Style": 1,
+        "People & Blogs": 1
+    },
+    "El Salvador": {
+        "Gaming": 1
+    },
+    "Finland": {
+        "Gaming": 1
+    },
+    "France": {
+        "Comedy": 1,
+        "Gaming": 1,
+        "Music": 2,
+        "People & Blogs": 1
+    },
+    "Germany": {
+        "Education": 1,
+        "Entertainment": 2,
+        "People & Blogs": 1,
+        "Science & Technology": 1
+    },
+    "India": {
+        "Comedy": 16,
+        "Education": 15,
+        "Entertainment": 43,
+        "Film & Animation": 11,
+        "Gaming": 2,
+        "Howto & Style": 6,
+        "Movies": 2,
+        "Music": 29,
+        "News & Politics": 11,
+        "People & Blogs": 15,
+        "Science & Technology": 4,
+        "Shows": 5,
+        "Trailers": 2
+    },
+    "Indonesia": {
+        "Education": 3,
+        "Entertainment": 12,
+        "Gaming": 2,
+        "Music": 1,
+        "News & Politics": 1,
+        "People & Blogs": 8
+    },
+    "Iraq": {
+        "Comedy": 1,
+        "Music": 1
+    },
+    "Italy": {
+        "Entertainment": 2
+    },
+    "Japan": {
+        "Comedy": 1,
+        "Entertainment": 3,
+        "Gaming": 1
+    },
+    "Jordan": {
+        "Entertainment": 2,
+        "Music": 1
+    },
+    "Kuwait": {
+        "Gaming": 1
+    },
+    "Latvia": {
+        "Comedy": 1
+    },
+    "Malaysia": {
+        "Film & Animation": 1
+    },
+    "Mexico": {
+        "Comedy": 2,
+        "Education": 2,
+        "Entertainment": 6,
+        "Film & Animation": 1,
+        "Gaming": 3,
+        "Howto & Style": 4,
+        "Music": 2,
+        "News & Politics": 1,
+        "People & Blogs": 8,
+        "Travel & Events": 1
+    },
+    "Morocco": {
+        "Music": 1
+    },
+    "Netherlands": {
+        "Gaming": 1,
+        "Howto & Style": 1,
+        "Music": 1
+    },
+    "Pakistan": {
+        "Entertainment": 3,
+        "News & Politics": 1
+    },
+    "Peru": {
+        "Entertainment": 1
+    },
+    "Philippines": {
+        "Entertainment": 3,
+        "Music": 1,
+        "News & Politics": 3,
+        "People & Blogs": 3
+    },
+    "Russia": {
+        "Comedy": 1,
+        "Entertainment": 3,
+        "Film & Animation": 1,
+        "Gaming": 3,
+        "People & Blogs": 7
+    },
+    "Samoa": {
+        "Music": 1
+    },
+    "Saudi Arabia": {
+        "Entertainment": 2,
+        "Gaming": 2,
+        "Music": 1,
+        "People & Blogs": 2,
+        "Shows": 1
+    },
+    "Singapore": {
+        "Entertainment": 1
+    },
+    "South Korea": {
+        "Entertainment": 4,
+        "Gaming": 1,
+        "Music": 7,
+        "People & Blogs": 3
+    },
+    "Spain": {
+        "Comedy": 2,
+        "Education": 1,
+        "Entertainment": 4,
+        "Gaming": 8,
+        "Music": 3,
+        "People & Blogs": 1,
+        "Sports": 2
+    },
+    "Sweden": {
+        "Gaming": 1,
+        "Howto & Style": 1,
+        "Music": 1
+    },
+    "Switzerland": {
+        "Sports": 1
+    },
+    "Thailand": {
+        "Comedy": 2,
+        "Entertainment": 9,
+        "Gaming": 1,
+        "Music": 5,
+        "News & Politics": 1
+    },
+    "Turkey": {
+        "Entertainment": 2,
+        "Music": 1
+    },
+    "Ukraine": {
+        "Entertainment": 5,
+        "Howto & Style": 1,
+        "News & Politics": 1,
+        "Shows": 1
+    },
+    "United Arab Emirates": {
+        "Autos & Vehicles": 1,
+        "Comedy": 1,
+        "Gaming": 1,
+        "People & Blogs": 2
+    },
+    "United Kingdom": {
+        "Comedy": 1,
+        "Entertainment": 10,
+        "Film & Animation": 2,
+        "Gaming": 7,
+        "Howto & Style": 1,
+        "Music": 14,
+        "News & Politics": 1,
+        "People & Blogs": 2,
+        "Pets & Animals": 1,
+        "Science & Technology": 1,
+        "Shows": 1,
+        "Sports": 1
+    },
+    "United States": {
+        "Comedy": 19,
+        "Education": 16,
+        "Entertainment": 68,
+        "Film & Animation": 10,
+        "Gaming": 26,
+        "Howto & Style": 12,
+        "Music": 73,
+        "News & Politics": 5,
+        "Nonprofits & Activism": 1,
+        "People & Blogs": 37,
+        "Pets & Animals": 2,
+        "Science & Technology": 5,
+        "Shows": 4,
+        "Sports": 5
+    },
+    "Venezuela": {
+        "People & Blogs": 1
+    },
+    "Vietnam": {
+        "Comedy": 1,
+        "Entertainment": 1,
+        "Music": 1
+    }
+};
